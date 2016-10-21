896d413f55f617e886705eae0e7d976c5618d539
jaysix79 <jaysix79@gmail.com>
2016-10-20 22:50:03 -0700

    Add files via upload

---
+++ Install_WebIOPi.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+## Bash Script to clear cached memory on (Ubuntu/Debian) Linux
+##	sudo /home/pi/programs/install/Install_WebIOPi.sh 
+### BEGIN INIT INFO
+# Provides:          Install_WebIOPi
+# Required-Start:    $local_fs 
+# Required-Stop:     $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Install_WebIOPi
+### END INIT INFO
+LOG_FILE="/home/pi/log/install/Install_WebIOPi.log"
+
+###	CHECKING IF IS BEING RUN AS ROOT
+if [ "$(whoami)" != "root" ]
+then
+	echo "You have to run this script as Superuser! in order to install Install_WebIOPi"					| tee -a 	"$LOG_FILE"
+	exit 0
+fi
+
+###	CHECKING IF THE FOLDER EXIST
+if [ -d "/home/pi/WebIOPi-0.7.1d" ]; then
+	echo "install Install_WebIOPi is already installed......."												| tee -a 	"$LOG_FILE"
+	exit 0
+fi
+
+###	IF FOLDER DOESNT EXIT THEN LETS INSTALL
+if [ ! -d "/home/pi/WebIOPi-0.7.1d" ]; then
+	echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #"	| tee -a 	"$LOG_FILE"
+	echo "# # # # # # # # Starting apt-get-Install_WebIOPi  # # # # # # # # # # # # # # # # # # #"			| tee -a 	"$LOG_FILE"
+	date 																									| tee -a 	"$LOG_FILE"
+	echo																									| tee -a 	"$LOG_FILE"
+
+	cd /home/pi
+	wget http://sourceforge.net/projects/webiopi/files/WebIOPi-0.7.1.tar.gz 								| tee -a 	"$LOG_FILE"
+	tar xvzf /home/pi/WebIOPi-0.7.1.tar.gz 																	| tee -a 	"$LOG_FILE"
+	cd /home/pi/WebIOPi-0.7.1																				| tee -a 	"$LOG_FILE"
+	sudo /home/pi/WebIOPi-0.7.1/setup.sh 																	| tee -a 	"$LOG_FILE"
+	rm /home/pi/WebIOPi-0.7.1.tar.gz																		| tee -a 	"$LOG_FILE"
+	sudo update-rc.d webiopi defaults																		| tee -a 	"$LOG_FILE"
+	echo																									| tee -a 	"$LOG_FILE"
+
+	echo "Congratuation install Install_WebIOPi is now installed......." 									| tee -a 	"$LOG_FILE"
+	echo																									| tee -a 	"$LOG_FILE"
+	echo																									| tee -a 	"$LOG_FILE"
+	date																									| tee -a 	"$LOG_FILE"
+	echo "# # # # # # # #        Install_WebIOPi DONE!!       # # # # # # # # # # # # # # # # # #"			| tee -a 	"$LOG_FILE"
+	echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #"	| tee -a 	"$LOG_FILE"
+	echo																									| tee -a 	"$LOG_FILE"
+	echo																									| tee -a 	"$LOG_FILE"
+	echo																									| tee -a 	"$LOG_FILE"
+	echo																									| tee -a 	"$LOG_FILE"
+	echo																									| tee -a 	"$LOG_FILE"
+	echo																									| tee -a 	"$LOG_FILE"
+	
+	
+	exit 0
+fi
+
+
+
+
+
+
+
+
+exit 0
---
+++ Install_blynk_latest.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+## Bash Script to clear cached memory on (Ubuntu/Debian) Linux
+##	sudo /home/pi/programs/install/Install_blynk_latest.sh 
+##	to run blynk "node blynk.js 2f79d6db732040449192407b49934622"
+##	auto start add this "/usr/local/lib/node_modules/blynk-library/blynk.js 2f79d6db732040449192407b49934622" on "sudo nano /etc/rc.local"
+### BEGIN INIT INFO
+# Provides:          Install_blynk_latest
+# Required-Start:    $local_fs 
+# Required-Stop:     $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Install_blynk_latest
+### END INIT INFO
+LOG_FILE="/home/pi/log/install/Install_blynk_latest.log"
+
+###	CHECKING IF IS BEING RUN AS ROOT
+if [ "$(whoami)" != "root" ]; then
+	echo "You have to run this script as Superuser! in order to install Install_blynk_latest"
+	exit 1
+fi
+
+###	CHECKING IF THE FOLDER EXIST
+if [ -d "/home/pi/blynk-libraryX" ]; then
+	echo "install Install_blynk_latest is already installed......."
+	exit 1
+fi
+
+###	IF FOLDER DOESNT EXIT THEN LETS INSTALL
+if [ ! -d "/home/pi/blynk-libraryX" ]; then
+	echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #"	| tee -a 	"$LOG_FILE"
+	echo "# # # # # # # # Starting apt-get-Install_blynk_latest  # # # # # # # # # # # # # # # # # # #"		| tee -a 	"$LOG_FILE"
+	date 																									| tee -a 	"$LOG_FILE"
+	echo																									| tee -a 	"$LOG_FILE"
+
+	
+	apt-get update && sudo apt-get upgrade																	| tee -a 	"$LOG_FILE"
+	curl -sLS https://apt.adafruit.com/add | sudo bash														| tee -a 	"$LOG_FILE"
+	sudo apt-get install node																				| tee -a 	"$LOG_FILE"
+	sudo node -v																							| tee -a 	"$LOG_FILE"
+	apt-get install build-essential																			| tee -a 	"$LOG_FILE"
+	sudo npm install -g npm																					| tee -a 	"$LOG_FILE"
+	sudo npm install -g onoff																				| tee -a 	"$LOG_FILE"
+	sudo npm install -g blynk-library																		| tee -a 	"$LOG_FILE"
+
+	echo ""																									| tee -a 	"$LOG_FILE"
+
+
+	echo "Congratuation install Install_blynk_latest is now installed......." 								| tee -a 	"$LOG_FILE"
+	echo																									| tee -a 	"$LOG_FILE"
+	date																									| tee -a 	"$LOG_FILE"
+	echo "# # # # # # # #        Install_blynk_latest DONE!!       # # # # # # # # # # # # # # # # # #"		| tee -a 	"$LOG_FILE"
+	echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #"	| tee -a 	"$LOG_FILE"
+	echo																									| tee -a 	"$LOG_FILE"
+	echo																									| tee -a 	"$LOG_FILE"
+	echo																									| tee -a 	"$LOG_FILE"
+	echo																									| tee -a 	"$LOG_FILE"
+	echo																									| tee -a 	"$LOG_FILE"
+	echo																									| tee -a 	"$LOG_FILE"
+
+	exit 1
+fi
+
+
+
+
+
+
+
+
+exit 1
