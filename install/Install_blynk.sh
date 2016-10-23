79f988d7f9964d1f15873d5a2f781421ba9d4a67
jaysix79 <jaysix79@gmail.com>
2016-10-22 20:52:23 -0700

    Update Install_blynk.sh

--- install/Install_blynk.sh
+++ install/Install_blynk.sh
@@ -13,42 +13,42 @@ LOG_FILE="/home/pi/log/install/Install_blynk.log"
 
 ###	CHECKING IF IS BEING RUN AS ROOT
 if [ "$(whoami)" != "root" ]; then
-	echo "You have to run this script as Superuser! in order to install Install_blynk"
+	echo "You have to run this script as Superuser! in order to install Install_blynk"			| tee -a 	"$LOG_FILE"
 	exit 1
 fi
 
 ###	CHECKING IF THE FOLDER EXIST
 if [ -d "/home/pi/blynk-library" ]; then
-	echo "install Install_blynk is already installed......."
+	echo "install Install_blynk is already installed......."						| tee -a 	"$LOG_FILE"
 	exit 1
 fi
 
 ###	IF FOLDER DOESNT EXIT THEN LETS INSTALL
 if [ ! -d "/home/pi/blynk-library" ]; then
 	echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #"	| tee -a 	"$LOG_FILE"
-	echo "# # # # # # # # Starting apt-get-Install_blynk  # # # # # # # # # # # # # # # # # # #"			| tee -a 	"$LOG_FILE"
-	date 																									| tee -a 	"$LOG_FILE"
-	echo																									| tee -a 	"$LOG_FILE"
+	echo "# # # # # # # # 	Starting apt-get-Install_blynk		# # # # # # # # # # # # # # # # # #"	| tee -a 	"$LOG_FILE"
+	date 													| tee -a 	"$LOG_FILE"
+	echo ""													| tee -a 	"$LOG_FILE"
 
-	cd /home/pi 																							| tee -a 	"$LOG_FILE"
-	git clone https://github.com/blynkkk/blynk-library.git 													| tee -a 	"$LOG_FILE"
-	cd /home/pi/blynk-library/linux 																		| tee -a 	"$LOG_FILE"
-	make clean all target=raspberry 																		| tee -a 	"$LOG_FILE"
-	echo ""																									| tee -a 	"$LOG_FILE"
+	git clone https://github.com/blynkkk/blynk-library.git 							| tee -a 	"$LOG_FILE"
+	make clean all target=raspberry 									| tee -a 	"$LOG_FILE"
+	echo ""													| tee -a 	"$LOG_FILE"
 
 
 	echo "Congratuation install Install_blynk is now installed......." 										| tee -a 	"$LOG_FILE"
 	echo																									| tee -a 	"$LOG_FILE"
 	date																									| tee -a 	"$LOG_FILE"
-	echo "# # # # # # # #        Install_blynk DONE!!       # # # # # # # # # # # # # # # # # #"			| tee -a 	"$LOG_FILE"
+	echo "# # # # # # # #       	 Install_blynk DONE!!   	# # # # # # # # # # # # # # # # # #"	| tee -a 	"$LOG_FILE"
 	echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #"	| tee -a 	"$LOG_FILE"
-	echo																									| tee -a 	"$LOG_FILE"
-	echo																									| tee -a 	"$LOG_FILE"
-	echo																									| tee -a 	"$LOG_FILE"
-	echo																									| tee -a 	"$LOG_FILE"
-	echo																									| tee -a 	"$LOG_FILE"
-	echo																									| tee -a 	"$LOG_FILE"
-	echo																									| tee -a 	"$LOG_FILE"
+	echo ""													| tee -a 	"$LOG_FILE"
+	echo ""													| tee -a 	"$LOG_FILE"
+	echo ""													| tee -a 	"$LOG_FILE"
+	echo ""													| tee -a 	"$LOG_FILE"echo ""													| tee -a 	"$LOG_FILE"echo ""													| tee -a 	"$LOG_FILE"
+	echo ""													| tee -a 	"$LOG_FILE"
+	echo ""													| tee -a 	"$LOG_FILE"
+	echo ""													| tee -a 	"$LOG_FILE"
+	echo ""													| tee -a 	"$LOG_FILE"
+	echo ""													| tee -a 	"$LOG_FILE"
 	
 	exit 1
 fi
