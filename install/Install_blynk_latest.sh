d0b4625d171ef487cb57238d998b9d2874838469
jaysix79 <jaysix79@gmail.com>
2016-11-06 17:04:20 -0800

    Update Install_blynk_latest.sh

--- install/Install_blynk_latest.sh
+++ install/Install_blynk_latest.sh
@@ -13,51 +13,51 @@
 ### END INIT INFO
 
 mkdir -p /home/pi/log/install
-LOG_FILE="/home/pi/log/install/Install_blynk_latest.log"
+LOG_FILE="/home/pi/log/install/Install_blynk_latest.log"							| tee -a 	"$LOG_FILE"
 
 ###	CHECKING IF IS BEING RUN AS ROOT
 if [ "$(whoami)" != "root" ]; then
-	echo "You have to run this script as Superuser! in order to install Install_blynk_latest"
+	echo "You have to run this script as Superuser! in order to install Install_blynk_latest"		| tee -a 	"$LOG_FILE"
 	exit 1
 fi
 
 ###	CHECKING IF THE FOLDER EXIST
 if [ -d "/home/pi/blynk-libraryX" ]; then
-	echo "install Install_blynk_latest is already installed......."
+	echo "install Install_blynk_latest is already installed......."						| tee -a 	"$LOG_FILE"
 	exit 1
 fi
 
 ###	IF FOLDER DOESNT EXIT THEN LETS INSTALL
 if [ ! -d "/home/pi/blynk-libraryX" ]; then
 	echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #"	| tee -a 	"$LOG_FILE"
-	echo "# # # # # # # # Starting apt-get-Install_blynk_latest  # # # # # # # # # # # # # # # # # # #"		| tee -a 	"$LOG_FILE"
-	date 																									| tee -a 	"$LOG_FILE"
-	echo																									| tee -a 	"$LOG_FILE"
+	echo "# # # # # # # # Starting apt-get-Install_blynk_latest  # # # # # # # # # # # # # # # # # # #"	| tee -a 	"$LOG_FILE"
+	date 													| tee -a 	"$LOG_FILE"
+	echo													| tee -a 	"$LOG_FILE"
 
 	
-	apt-get update && sudo apt-get upgrade																	| tee -a 	"$LOG_FILE"
-	curl -sLS https://apt.adafruit.com/add | sudo bash														| tee -a 	"$LOG_FILE"
-	sudo apt-get install node																				| tee -a 	"$LOG_FILE"
-	sudo node -v																							| tee -a 	"$LOG_FILE"
-	apt-get install build-essential																			| tee -a 	"$LOG_FILE"
-	sudo npm install -g npm																					| tee -a 	"$LOG_FILE"
-	sudo npm install -g onoff																				| tee -a 	"$LOG_FILE"
-	sudo npm install -g blynk-library																		| tee -a 	"$LOG_FILE"
+	
+	curl -sLS https://apt.adafruit.com/add | sudo bash							| tee -a 	"$LOG_FILE"
+	sudo apt-get install node										| tee -a 	"$LOG_FILE"
+	sudo node -v												| tee -a 	"$LOG_FILE"
+	apt-get install build-essential										| tee -a 	"$LOG_FILE"
+	sudo npm install -g npm											| tee -a 	"$LOG_FILE"
+	sudo npm install -g onoff										| tee -a 	"$LOG_FILE"
+	sudo npm install -g blynk-library									| tee -a 	"$LOG_FILE"
 
-	echo ""																									| tee -a 	"$LOG_FILE"
+	echo ""													| tee -a 	"$LOG_FILE"
 
 
-	echo "Congratuation install Install_blynk_latest is now installed......." 								| tee -a 	"$LOG_FILE"
-	echo																									| tee -a 	"$LOG_FILE"
-	date																									| tee -a 	"$LOG_FILE"
-	echo "# # # # # # # #        Install_blynk_latest DONE!!       # # # # # # # # # # # # # # # # # #"		| tee -a 	"$LOG_FILE"
+	echo "Congratuation install Install_blynk_latest is now installed......." 				| tee -a 	"$LOG_FILE"
+	echo													| tee -a 	"$LOG_FILE"
+	date													| tee -a 	"$LOG_FILE"
+	echo "# # # # # # # #        Install_blynk_latest DONE!!       # # # # # # # # # # # # # # # # # #"	| tee -a 	"$LOG_FILE"
 	echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #"	| tee -a 	"$LOG_FILE"
-	echo																									| tee -a 	"$LOG_FILE"
-	echo																									| tee -a 	"$LOG_FILE"
-	echo																									| tee -a 	"$LOG_FILE"
-	echo																									| tee -a 	"$LOG_FILE"
-	echo																									| tee -a 	"$LOG_FILE"
-	echo																									| tee -a 	"$LOG_FILE"
+	echo													| tee -a 	"$LOG_FILE"
+	echo													| tee -a 	"$LOG_FILE"
+	echo													| tee -a 	"$LOG_FILE"
+	echo													| tee -a 	"$LOG_FILE"
+	echo													| tee -a 	"$LOG_FILE"
+	echo													| tee -a 	"$LOG_FILE"
 
 	exit 1
 fi
