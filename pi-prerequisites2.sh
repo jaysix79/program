40c8a002d6cc6132cc4fa948cf2d1a46c1ddf6a9
jaysix79 <jaysix79@gmail.com>
2016-11-18 10:42:52 -0800

    Update pi-prerequisites2.sh

--- pi-prerequisites2.sh
+++ pi-prerequisites2.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 ## Bash Script to clear cached memory on (Ubuntu/Debian) Linux sudo resize2fs /dev/sda2
-##	sudo sh /home/pi/programs/pi-prerequisites2.sh
+##	sudo sh /home/pi/programs/pi-prerequisites.sh
 #	sudo apt-get install git
 #	git clone https://github.com/jaysix79/programs.git
 #
 ### BEGIN INIT INFO
-# Provides:          pi-prerequisites2.sh
+# Provides:          pi-prerequisites.sh
 # Required-Start:    $local_fs 
 # Required-Stop:     $local_fs
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: pi-prerequisites2.sh
+# Short-Description: pi-prerequisites.sh
 ### END INIT INFO
 
 mkdir -p /home/pi/log/install
-LOG_FILE="/home/pi/log/install/pi-prerequisites2.log"
+LOG_FILE="/home/pi/log/install/pi-prerequisites.log"
 
 ###	CHECKING IF IS BEING RUN AS ROOT
 if [ "$(whoami)" != "root" ]; then
@@ -26,15 +26,25 @@ if [ "$(whoami)" != "root" ]; then
 	exit 0
 fi
 
-
-
-
-
-
+###	CHECKING IF THE FOLDER EXIST
+if [ -f "/usr/local/sbin/pi_garage_alert.py" ]; then
+	service pi_garage_alert status										| tee -a  	"$LOG_FILE"
+	echo ""													| tee -a  	"$LOG_FILE"
+	echo ""													| tee -a  	"$LOG_FILE"
+	echo "install Install_pi_garage_alert is already installed......."					| tee -a  	"$LOG_FILE"
+	echo ""													| tee -a  	"$LOG_FILE"
+	echo ""													| tee -a  	"$LOG_FILE"
+exit 0
+fi
 
 ###	IF FOLDER DOESNT EXIT THEN LETS INSTALL
 if [ ! -d "/xxxx" ]; then
-	echo "# # # # # # # #        pi-prerequisites2.sh DONE!!         # # # # # # # # # # # # # # # # # #"	| tee -a 	"$LOG_FILE"
+	echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #"	| tee -a 	"$LOG_FILE"
+	echo "# # # # # # # #  Starting apt-get pi-prerequisites.sh   # # # # # # # # # # # # # # # # # # #"	| tee -a 	"$LOG_FILE"
+	
+	
+	
+	echo "# # # # # # # #        pi-prerequisites.sh DONE!!         # # # # # # # # # # # # # # # # # #"	| tee -a 	"$LOG_FILE"
 	echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #"	| tee -a 	"$LOG_FILE"
 	echo "" 												| tee -a 	"$LOG_FILE"
 	echo "" 												| tee -a 	"$LOG_FILE"
@@ -46,8 +56,8 @@ if [ ! -d "/xxxx" ]; then
 	echo "" 												| tee -a 	"$LOG_FILE"
 	
 	exit 1
-
 fi
+
 
 
 
