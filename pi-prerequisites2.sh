081d29fbd0f39951dc70472ba6c316b276730836
jaysix79 <jaysix79@gmail.com>
2016-11-18 10:37:54 -0800

    Update pi-prerequisites2.sh

--- pi-prerequisites2.sh
+++ pi-prerequisites2.sh
@@ -30,19 +30,6 @@ fi
 
 
 
-confirm () {
-    # call with a prompt string or use a default
-    read -r -p "${1:-Are you sure? [y/N]} " response
-    case $response in
-        [yY][eE][sS]|[yY]) 
-            echo this is YES
-            ;;
-        *)
-            echo this is NO
-            ;;
-    esac
-}
-
 
 
 
