4742732af48b5b17c3e59a373d3aa317e68855e0
jaysix79 <jaysix79@gmail.com>
2016-11-18 17:53:21 -0800

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -74,7 +74,7 @@ if [ ! -d "/xxxx" ]; then
 	read -r -p "Are You Sure you want to continue? [Y/n] " input
 	case $input in
 	    [yY][eE][sS]|[yY])
-			/home/pi/programs/static_ip.sh 								| tee -a 	"$LOG_FILE"
+			###/home/pi/programs/static_ip.sh 								| tee -a 	"$LOG_FILE"
 			echo "" 										| tee -a 	"$LOG_FILE"
 			echo "" 										| tee -a 	"$LOG_FILE"
 			echo "Successfully changed IP address  to 192.168.xxx.5"							| tee -a 	"$LOG_FILE"
